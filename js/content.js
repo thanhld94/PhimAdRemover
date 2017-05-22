5a910b8fb0b8953581ece828b6267248d5b0629b
thanhld94 <thanhld94@gmail.com>
2017-05-22 10:23:03 -0500

    Removed uncessary code

--- js/content.js
+++ js/content.js
@@ -1,8 +1,5 @@
 var script = document.createElement('script');
 
 script.src = chrome.extension.getURL('js/adRemover.js');
-script.onLoad = function() {
-  console.log("here");
-};
 
 (document.head || document.documentElement).appendChild(script);
