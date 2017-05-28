8b70dfe24c1bc213f2fe15331690e843aac6d92d
thanhld94 <thanhld94@gmail.com>
2017-05-28 11:45:46 -0500

    Added removeAd action to respond to changing episodes

--- js/adRemover.js
+++ js/adRemover.js
@@ -1,5 +1,15 @@
-try {
-  document.getElementById("preroll-overlay").remove();
-} catch (error) {
-  console.log("Some error!");
+window.addEventListener("load", removeAd);
+
+episodes = document.querySelectorAll(".episode > a");
+episodes.forEach(function(episode) {
+  episode.addEventListener("click", removeAd);
+});
+
+/** Remove the flash-overlay ad element */
+function removeAd() {
+  try {
+    document.querySelector("#preroll-overlay").remove();
+  } catch (error) {
+    console.log("Some error!");
+  }
 }
